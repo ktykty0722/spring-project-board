71dd04a25ec8e8dc65e4fe707e65f1e8140a3a27
ktykty0722 <ktykty0722@naver.com>
2023-04-26 21:24:14 +0900

    initial commit

---
+++ src/main/java/com/fastcampus/springprojectboard/SpringProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.springprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/springprojectboard/SpringProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.springprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
